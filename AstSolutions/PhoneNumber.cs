d8b318124749c8cba7aa137ee5075af1b0be3e34
Ewan Thompson <ewanpt@gmail.com>
2020-04-25 14:46:02 +0800

    Add unit testing for PhoneNumber class

---
+++ AstSolutions/PhoneNumber.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AstSolutions
+{
+    public static class PhoneNumber
+    {
+        public static bool IsValidPhonenumber(string phoneNumber)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
