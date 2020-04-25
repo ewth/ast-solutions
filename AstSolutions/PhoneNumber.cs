31a5579e603dd3cc14573e178e7e46397f490138
Ewan Thompson <ewanpt@gmail.com>
2020-04-25 15:17:01 +0800

    Remove dubious validation of +6104

--- AstSolutions/PhoneNumber.cs
+++ AstSolutions/PhoneNumber.cs
@@ -16,14 +16,14 @@ namespace AstSolutions
         /// <param name="phoneNumber"></param>
         /// <returns>True if valid, False otherwise</returns>
         /// <remarks>The string is sanitised first so formatting doesn't become an issue. E.g. "0412 123 123", "0412123123", "04 12 12 3 1 2 3" are all valid phone numbers, even if the formatting is horrible.</remarks>
-        /// <remarks>+61412123123 and +610412123123 are considered valid representations of the phone number. +6104 may not be "technically" valid but is how users sometimes enter phone numbers, from experience.</remarks>
+        /// <remarks>+61412123123 is valid but +610412123123 is considered invalid. Although I've seen customer records with numbers as "+6104" it's not technically correct, and thus fails validation here.</remarks>
         public static bool IsValidPhonenumber(string phoneNumber)
         {
 
             // Develop regex to match valid phone number
             // Remove any spaces or dashes from the phone number
             phoneNumber = Regex.Replace(phoneNumber, "[- ]", "");
-            return Regex.IsMatch(phoneNumber, "^(([+]?(614|6104))|04)([0-9]{8,8})$");
+            return Regex.IsMatch(phoneNumber, "^(([+]?614)|04)([0-9]{8,8})$");
         }
     }
 }
